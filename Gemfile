f99d6d4b4879bac82664fbf584c206780f50373d
myrual <lilin@myrual.me>
2017-04-26 10:34:48 +0000

    json login done

--- Gemfile
+++ Gemfile
@@ -41,6 +41,7 @@ gem 'mini_magick'
 gem 'figaro'
 gem 'fog'
 gem 'paperclip', '~> 4.1'
+gem 'simple_token_authentication', '~> 1.0' # see semver.org
 
 # Use Redis adapter to run Action Cable in production
 # gem 'redis', '~> 3.0'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  acts_as_token_authenticatable
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -50,5 +52,19 @@ class User < ApplicationRecord
 
   def display_name
     self.email.split("@").first
+  end
+  
+  
+   def reset_authentication_token!
+     self.authentication_token = generate_authentication_token("")
+     self.save
+   end
+ 
+  private
+  def generate_authentication_token(token_generator)
+    loop do
+      token = Devise.friendly_token
+      break token unless User.where(authentication_token: token).first
+    end
   end
 end
