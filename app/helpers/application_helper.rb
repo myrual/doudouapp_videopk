7be42b9472edb56bf67febd96dcdca69c2e8abd0
myrual <lilin@myrual.me>
2017-05-04 12:19:41 +0000

    fix bug

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 module ApplicationHelper
-    def full_title(page_title = '')
+    def full_title()
         base_title = "吃饭，睡觉，打斗斗"
-        if page_title.empty?
-            base_title
+        if @title
+            "斗斗" + " | " + @title
         else
-            "斗斗" + " | " + page_title
+            base_title
         end
     end
 end
