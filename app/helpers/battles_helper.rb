89dc50c7d7e5645bb510d3849c5c840fd5019705
myrual <lilin@myrual.me>
2017-05-08 06:43:48 +0000

    we support better jquery post

--- app/helpers/battles_helper.rb
+++ app/helpers/battles_helper.rb
@@ -17,4 +17,30 @@ module BattlesHelper
    def right_votes(battle)
       battle.right_followers.count + battle.visitor_votes.where(voteLeft: false).count
    end
+  def right_is_voted(battle)
+    if current_user
+      current_user.has_follow_right?(battle)
+    else
+      visitorID = find_visitor_id
+      battle.visitor_votes.find_by(visitorID:visitorID, voteLeft:false) != nil
+    end
+  end  
+  def left_is_voted(battle)
+    if current_user
+      current_user.has_follow_left?(battle)
+    else
+      visitorID = find_visitor_id
+      battle.visitor_votes.find_by(visitorID:visitorID, voteLeft:true) != nil
+    end
+  end
+  def find_visitor_id
+    visitorID = ""
+    if(visitor_id_incookie = cookies.signed[:visitorID])
+      visitorID = visitor_id_incookie
+    else
+      visitorID = Time.now.to_s
+      cookies.permanent.signed[:visitorID] = visitorID
+    end
+    visitorID
+  end
 end
