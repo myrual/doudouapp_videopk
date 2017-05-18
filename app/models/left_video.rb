d865afdd3df02aa5b33e9fb7e8f4d32842eccfcc
myrual <lilin@myrual.me>
2017-05-18 04:18:17 +0000

    add two model for easy

---
+++ app/models/left_video.rb
@@ -0,0 +1,14 @@
+class Video < ApplicationRecord
+  mount_uploader :image, ImageUploader
+  mount_uploader :video, VideoUploader
+  validates :title, presence: true
+
+  belongs_to :user
+  has_many :video_comments
+  has_many :comments_from_user, through: :video_comments, source: :user
+  has_many :challengevideos
+  has_many :battles
+
+  #has_many :favor_videos_relationships
+  #has_many :followers, through: :favor_videos_relationships, source: :user
+end
---
+++ app/models/right_video.rb
@@ -0,0 +1,6 @@
+class Right_Video < Video
+  has_many :battles
+
+  #has_many :favor_videos_relationships
+  #has_many :followers, through: :favor_videos_relationships, source: :user
+end
