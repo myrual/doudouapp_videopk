e6760191576db9e0deeb2112e0b9f1d5905f8384
myrual <lilin@myrual.me>
2017-05-25 11:24:13 +0000

    done

--- config/routes.rb
+++ config/routes.rb
@@ -87,6 +87,9 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
+      
+      get 'upyunauths' => 'upyunauths#index'
+      
       resources :streams
       resources :videos do
         member do
