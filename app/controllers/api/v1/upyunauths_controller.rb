b9b7ff83746be616d3e0585b55b8c422c03332d0
myrual <lilin@myrual.me>
2017-05-25 12:22:01 +0000

    remove json verify for upyun

--- app/controllers/api/v1/upyunauths_controller.rb
+++ app/controllers/api/v1/upyunauths_controller.rb
@@ -2,15 +2,11 @@ require 'base64'
 require 'cgi'
 require 'openssl'
 class Api::V1::UpyunauthsController < ApplicationController
-    skip_before_action :verify_authenticity_token, if: :json_request?
-    
     def json_request?
       request.format.json?
     end
     def index
       
-      if verify_api_only == true
-        
         respond_to :json
         md5 = Digest::MD5.new
         md5.update ENV["UPYUN_OPERATOR_PASSWORD"] #update 又拍云的密码
@@ -18,12 +14,5 @@ class Api::V1::UpyunauthsController < ApplicationController
         data = params["data"]
         key = md5.hexdigest
         @result = Base64.encode64("#{OpenSSL::HMAC.digest('sha1', key, data)}") 
-      else
-        render status: :unauthorized
-      end
     end
-  private
-  def verify_api_only
-     params[:appid].present? and params[:appsecret].present?
-  end
 end
