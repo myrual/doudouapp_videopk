7560beaedc5ed2fbd6693a958cc0c80441e70259
myrual <lilin@myrual.me>
2017-05-31 13:31:40 +0000

    add protection for model topics

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,4 +1,8 @@
 class Topic < ApplicationRecord
+  validates :title, presence: true
+  validates :t1topic_id, presence: true
+  validates :running, presence: true
+  
     belongs_to :t1topic
     belongs_to :user
     has_many :topic_videos
