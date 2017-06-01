4a3ac5d66feca292492105f9c5b42888e82a8bc3
myrual <lilin@myrual.me>
2017-06-01 11:44:08 +0000

    fix bug and improve ui

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,6 +5,7 @@ Rails.application.config.assets.version = '1.0'
 Rails.application.config.assets.precompile += %w( google_analytics.js)
 Rails.application.config.assets.precompile += %w( video-js.css)
 Rails.application.config.assets.precompile += %w( video.js)
+Rails.application.config.assets.precompile += %w( bootstrap-filestyle.min.js)
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
 
