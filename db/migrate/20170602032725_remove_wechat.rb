be3769b614ca6c005cdcc41f29b7f5e46c232ba1
myrual <lilin@myrual.me>
2017-06-02 03:30:02 +0000

    rollback

--- db/migrate/20170602032725_remove_wechat.rb
+++ db/migrate/20170602032725_remove_wechat.rb
@@ -1,6 +1,6 @@
 require_relative "20170602031033_create_wechat_sessions"
 class RemoveWechat < ActiveRecord::Migration[5.0]
   def change
-    revert 20170602031033_create_wechat_sessions
+    revert CreateWechatSessions
   end
 end
