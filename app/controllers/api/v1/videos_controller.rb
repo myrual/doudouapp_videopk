8eda9037528feda897a5ebd346c12d49e38b188b
myrual <lilin@myrual.me>
2017-06-09 15:04:21 +0000

    update to latest model helper function for poster and video url

--- app/controllers/api/v1/videos_controller.rb
+++ app/controllers/api/v1/videos_controller.rb
@@ -10,7 +10,7 @@ class Api::V1::VideosController < ApplicationController
         current_wxuser = User.find(params[:user_id])
         videos = current_wxuser.videos.all
         @videos = videos.map {|each|
-          {:id => each.id, :title => each.title, :user_id => each.user_id, :origin_video_poster => each.image.thumb.to_s,:origin_video_url => each.video_url.to_s}
+          {:id => each.id, :title => each.title, :user_id => each.user_id, :video_poster => each.poster_url,:video_url => each.unique_url}
         }
       else
         render status: :unauthorized
@@ -22,7 +22,7 @@ class Api::V1::VideosController < ApplicationController
         respond_to :json
         current_wxuser = User.find(params[:user_id])
         each = current_wxuser.videos.find(params[:id])
-          @video = {:id => each.id, :title => each.title, :user_id => each.user_id, :origin_video_poster => each.image.thumb.to_s,:origin_video_url => each.video_url.to_s, :ext => each.ext_video != nil}
+          @video = {:id => each.id, :title => each.title, :user_id => each.user_id, :video_poster => each.poster_url,:video_url => each.unique_url}
       else
         render status: :unauthorized
       end
